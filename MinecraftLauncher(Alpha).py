b53303ac6dc49844c1e5c836c3a2c88f787c82fe
lolminiyt <ignaciololmini@gmail.com>
2024-03-15 23:18:24 +0000

    Update MinecraftLauncher(Alpha).py

--- MinecraftLauncher(Alpha).py
+++ MinecraftLauncher(Alpha).py
@@ -1,7 +1,6 @@
 import customtkinter as ctk
 import minecraft_launcher_lib
 import os
-import os
 import subprocess
 import sys
 import uuid
@@ -13,6 +12,7 @@ ASSETS_DIR = r"./assets"
 NATIVES_DIR = r"./natives"
 JAVA_HOME = r"./runtime/java11/"
 minecraft_directory = r"./.Minecraft"
+current_max = 0
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
@@ -54,34 +54,62 @@ def generate_uuid(username: str, uuid_dict: dict) -> str:
     new_uuid = uuid.uuid5(uuid.NAMESPACE_DNS, username)
     uuid_dict[username] = new_uuid  
     return new_uuid
+########################################
+def set_status(status: str):
+    print(status)
 
+def set_progress(progress: int):
+    global Download_Progress
+    if current_max != 0:
+        percent = progress / current_max
+        Download_Progress.set(percent)
+        root.update_idletasks()
+        
+def set_max(new_max: int):
+    global current_max
+    current_max = new_max
+################################################################################
 def Download_Play():
     username = username_textbox.get('1.0', 'end').strip() 
-    version = version_combobox.get() 
-    install_thread = threading.Thread(target=minecraft_launcher_lib.install.install_minecraft_version, args=(version, minecraft_directory))
-    install_thread.start()
-    install_thread.join()
-    uuid = generate_uuid(username, uuid_dict)
-    print(uuid)
-    options = {
-        "username": username,
-        "uuid": str(uuid),  
-        "token": "T",
-    }   
-    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, minecraft_directory, options)
+    version = version_combobox.get()
+
+    def install():
+        Play.configure(state="disabled") 
+        callback = {
+            "setStatus": set_status,
+            "setProgress": set_progress,
+            "setMax": set_max
+        }
+        minecraft_launcher_lib.install.install_minecraft_version(version, minecraft_directory, callback=callback)
+        root.after(0, launch)
+
+    def launch():
+        uuid = generate_uuid(username, uuid_dict)
+        print(uuid)
+        options = {
+            "username": username,
+            "uuid": str(uuid),  
+            "token": "T",
+        }   
+        minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, minecraft_directory, options)
  
-    subprocess.call(minecraft_command)
+        subprocess.call(minecraft_command)
  
-    sys.exit(0)
-  
+        sys.exit(0)
+    
+    threading.Thread(target=install).start()
+ ################################################################################
 
 label, username_textbox = CTL(root, "Username:", 20, 12)
 label, version_combobox = CTB(root, "Version:", 20, 47, version_list)
-rectangle_frame = ctk.CTkFrame(root, width=290, height=60, fg_color="#383838", corner_radius=0)
+rectangle_frame = ctk.CTkFrame(root, width=290, height=80, fg_color="#383838", corner_radius=0)
 rectangle_frame.place(x=0, y=89)
+Download_Progress = ctk.CTkProgressBar(root, orientation="horizontal", width=275, height=15, corner_radius=0, border_width=0)
+Download_Progress.place(x=0, y=89)
+Download_Progress.set(0)
 Play = ctk.CTkButton(root, text="Jugar", width=195 , corner_radius=0, border_color="#565B5E", fg_color="#333333", border_width=2, command=Download_Play)
-Play.place(x=16, y=105)
+Play.place(x=16, y=125)
 Settings = ctk.CTkButton(root, text="S", width=25 , corner_radius=0, border_color="#565B5E", fg_color="#333333", border_width=2)
-Settings.place(x=228, y=105)
+Settings.place(x=228, y=125)
 root.toplevel_window = None
 root.mainloop()
